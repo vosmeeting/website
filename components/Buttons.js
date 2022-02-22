d29f24ebe872a5dd152bc599be6cbf67fb9e9548
frvosm <neo@fauzans-MacBook-Pro.local>
2022-02-22 12:10:24 +0700

    add form validation

--- components/Buttons.js
+++ components/Buttons.js
@@ -5,7 +5,7 @@ const Button = ({ variant = 'primary', className, ...props }) => {
   return (
     <button
       className={cx(
-        'rounded-full py-2 whitespace-nowrap  hover:shadow-xl',
+        'rounded-full py-2 whitespace-nowrap  hover:shadow-xl disabled:bg-slate-400 text-slate-200 disabled:hover:shadow-none',
         {
           'text-vosm-blue ripple-bg-vosm-light-blue': variant === 'primary',
           'ripple-bg-slate-50 text-vosm-blue': variant === 'secondary',
--- pages/sponsor.tsx
+++ pages/sponsor.tsx
@@ -1,4 +1,6 @@
 import {
+  Banner,
+  Button,
   Card,
   ChoiceList,
   Form,
@@ -7,90 +9,205 @@ import {
   Layout,
   List,
   Page,
+  RadioButton,
+  Stack,
   TextContainer,
   TextField,
-  TextFieldProps,
 } from '@shopify/polaris'
-import { useCallback, useState } from 'react'
-import Button from '../components/Buttons'
 import {
-  BoothLocations,
+  asChoiceField,
+  lengthMoreThan,
+  notEmpty,
+  numericString,
+  useChoiceField,
+  useField,
+  useForm,
+} from '@shopify/react-form'
+import * as yup from 'yup'
+import {
   GeneralSupport,
   MarketingOpportunities,
   SponsorshipPreferences,
 } from './const'
 
-import { yupResolver } from '@hookform/resolvers/yup'
-import * as yup from 'yup'
-
-const schema = yup
-  .object({
-    companyName: yup.string().required(),
-    companyTelephone: yup.number().positive().integer().required(),
-    address: yup.string(),
-    fax: yup.string().optional(),
-    contactName: yup.string(),
-    email: yup.string().email(),
-    state: yup.string(),
-    city: yup.string(),
-  })
-  .required()
-
 export default function Sponsor() {
-  const handleSubmit = useCallback((_event) => setUrl(''), [])
+  const schema = {
+    companyName: useField({
+      value: '',
+      validates: [
+yup.string().required('this is required').validateSync
+      ],
+    }),
+    companyTelephone: useField({
+      value: '',
+      validates: [
+        yup
+          .string()
+          .required('please provide a company telephone number')
+          .matches(
+            /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im,
+            'phone number is invalid'
+          ).validateSync,
+      ],
+    }),
+    address: useField({
+      value: '',
+      validates: [notEmpty('address is required')],
+    }),
+    fax: useField({
+      value: '',
+      validates: [numericString('please provide a valid fax number')],
+    }),
+    contactName: useField({
+      value: '',
+      validates: [
+        yup.string().required('please provide a contact name')
+          .validateSync,
+      ],
+    }),
+    email: useField({
+      value: '',
+      validates: [
+        (input) =>
+          yup
+            .string()
+            .required('this is required')
+            .email('please provide a valid email')
+            .validateSync(input),
+      ],
+    }),
+    cityStateZip: useField({
+      value: '',
+      validates: [yup.string().required('this is required').validateSync],
+    }),
+    website: useField({
+      value: '',
+      validates: [yup.string().url('must be a valid url').validateSync],
+    }),
+    boothOption: useField<'1st'| '2nd'|'3rd' |'4th'>({
+      value: '1st',
+      validates: [
+        yup.string().required('this fields is required').validateSync,
+      ],
+    }),
+  }
 
-  const fields: TextFieldProps[] = [
-    { name: 'companyName', label: 'Company Name' },
-    { name: 'companyTelephone', label: 'Company Telephone', type: 'number' },
-    { name: 'address', label: 'Address' },
-    { name: 'contactName', label: "Contact's Name" },
-    { name: 'fax', label: 'Fax', type: 'number' },
-    { name: 'telephone', label: 'Telephone', type: 'number' },
-    { name: 'companyTelephone', label: 'Company Telephone', type: 'number' },
-    { label: 'City/State/Zip' },
-    { label: 'Website Address', placeholder: 'www.example.com', type: 'url' },
-  ]
+  const { fields, submit, submitting, dirty, submitErrors } = useForm({
+    fields: schema,
+    async onSubmit(form) {
+      await new Promise((resolve) => setTimeout(resolve, 1000))
+      console.log(form)
+      const remoteErrors: any = [] // your API call goes here
+      if (remoteErrors.length > 0) {
+        return { status: 'fail', errors: remoteErrors }
+      }
+
+      return { status: 'success' }
+    },
+  })
+
+  const errorBanner =
+    submitErrors.length > 0 ? (
+      <Layout.Section>
+        <Banner status="critical">
+          <p>There were some issues with your form submission:</p>
+          <ul>
+            {submitErrors.map(({ message }, index) => {
+              return <li key={`${message}${index}`}>{message}</li>
+            })}
+          </ul>
+        </Banner>
+      </Layout.Section>
+    ) : null
+
   return (
     <Page>
       <Layout>
         <Layout.Section>
           <Card title="Online store dashboard" sectioned>
-            <Form noValidate onSubmit={handleSubmit}>
+            <Form noValidate onSubmit={submit}>
+              {errorBanner}
               <FormLayout>
                 <FormLayout.Group>
-                  {fields.map((f, i) => {
-                    return (
-                      <TextField
-                        key={i}
-                        label={f.label}
-                        placeholder={f.placeholder}
-                      />
-                    )
-                  })}
+                  <TextField
+                    label="Company Name"
+                    placeholder="Acme corp"
+                    autoComplete="off"
+                    {...fields.companyName}
+                  />
+                  <TextField
+                    label="Company Telephone"
+                    placeholder="202-555-0124"
+                    autoComplete="tel"
+                    inputMode="tel"
+                    {...fields.companyTelephone}
+                  />
+                  <TextField
+                    label="Fax"
+                    autoComplete="fax"
+                    {...fields.fax}
+                  />
+                  <TextField
+                    label="Contact name"
+                    autoComplete="fullName"
+                    placeholder="John Anderson"
+                    {...fields.contactName}
+                  />
+                  <TextField
+                    label="Company/Contact Email"
+                    placeholder="john@company.mail.com"
+                    autoComplete="off"
+                    {...fields.email}
+                  />
+
+                  <TextField
+                    label="City/State/Zip"
+                    autoComplete="off"
+                    {...fields.cityStateZip}
+                  />
+
+                  <TextField
+                    placeholder="www.company.com"
+                    label="Company Website"
+                    autoComplete="off"
+                    {...fields.website}
+                  />
+                  <TextField
+                    multiline={4}
+                    label="Company Address"
+                    autoComplete="address"
+                    {...fields.address}
+                  />
                 </FormLayout.Group>
 
                 <TextContainer>
                   <Heading>Booth layout options</Heading>
                   <List>
                     <List.Item>
-                      Previous sponsors have priority in choosing booth space
+                      Previous sponsors have priority in
+                      choosing booth space
                     </List.Item>
                     <List.Item>
-                      Companies exhibiting at the exhibitor hall can get 2
-                      booths max
+                      Companies exhibiting at the
+                      exhibitor hall can get 2 booths max
                     </List.Item>
                     <List.Item>
-                      Companies exhibiting in the lecture hall can get 1 booth
-                      max
-                    </List.Item>
-                    <List.Item>Companies cannot share booths</List.Item>
-                    <List.Item>
-                      Registration fee is per booth and includes only one
-                      representative; additional representative = $350 [includes
-                      daily breakfast+ lunch and reception with hors D'oeuvres].
+                      Companies exhibiting in the lecture
+                      hall can get 1 booth max
                     </List.Item>
                     <List.Item>
-                      Booths are small. The limit is 2 reps per booth
+                      Companies cannot share booths
+                    </List.Item>
+                    <List.Item>
+                      Registration fee is per booth and
+                      includes only one representative;
+                      additional representative = $350
+                      [includes daily breakfast+ lunch and
+                      reception with hors D'oeuvres].
+                    </List.Item>
+                    <List.Item>
+                      Booths are small. The limit is 2
+                      reps per booth
                     </List.Item>
                   </List>
                 </TextContainer>
@@ -99,37 +216,29 @@ export default function Sponsor() {
                     'Booth Options (refer to booth floor layout sent over email)'
                   }
                 >
-                  <ChoiceList
-                    title={<b>Options:</b>}
-                    selected={['1st']}
-                    choices={['1st', '2nd', '3rd', '4th'].map((c) => ({
-                      label: `${c} option`,
-                      value: c,
-                    }))}
-                  />
-                  <ChoiceList
-                    title={<b>Locations</b>}
-                    selected={[]}
-                    choices={BoothLocations.map(({ name, price }, i) => ({
-                      label: name,
-                      helpText: price.toDollar(),
-                      value: name,
-                    }))}
-                  />
+                  <Stack vertical>
+                    {['1st', '2nd', '3rd', '4th'].map(choice => {
+                      return <RadioButton key={choice} label={`${choice} Options`} {...asChoiceField(fields.boothOption, choice)} />
+                    })}
+                  </Stack>
                 </FormLayout.Group>
                 <FormLayout.Group condensed>
                   <ChoiceList
                     selected={['0']}
                     title={<b>Sponsorship Prefferences</b>}
-                    choices={SponsorshipPreferences.map((s, i) => {
-                      return {
-                        label: s.name,
-                        helpText:
-                          `${s.price.toDollar()}` +
-                          (s.disc ? `, ${s.disc}% booth disc` : null),
-                        value: i.toString(),
+                    choices={SponsorshipPreferences.map(
+                      (s, i) => {
+                        return {
+                          label: s.name,
+                          helpText:
+                            `${s.price.toDollar()}` +
+                            (s.disc
+                              ? `, ${s.disc}% booth disc`
+                              : null),
+                          value: i.toString(),
+                        }
                       }
-                    })}
+                    )}
                   />
                   <ChoiceList
                     selected={['0']}
@@ -147,24 +256,35 @@ export default function Sponsor() {
                   <ChoiceList
                     title={<b>Marketing Opportunities</b>}
                     selected={['0']}
-                    choices={MarketingOpportunities.map((o, i) => {
-                      return {
-                        disabled: o.name.includes('*'), // disable exclusive package for now
-                        label: o.name,
-                        helpText: o.price.toDollar(),
-                        value: i.toString(),
+                    choices={MarketingOpportunities.map(
+                      (o, i) => {
+                        return {
+                          disabled: o.name.includes(
+                            '*'
+                          ), // disable exclusive package for now
+                          label: o.name,
+                          helpText: o.price.toDollar(),
+                          value: i.toString(),
+                        }
                       }
-                    })}
+                    )}
                   />
                 </FormLayout.Group>
               </FormLayout>
+
+              <TextContainer>
+                <p className="text-right italic text-slate-600">
+                  *=exclusive sponsorship
+                </p>
+              </TextContainer>
+              <Button
+                submit
+                // disabled={!dirty}
+                loading={submitting}
+              >
+                Proceed to payment
+              </Button>
             </Form>
-            <TextContainer>
-              <p className='text-right text-slate-600 italic'>*=exclusive sponsorship</p>
-            </TextContainer>
-            <Button type="submit" className={'px-4'}>
-              Proceed to Payment
-            </Button>
           </Card>
         </Layout.Section>
       </Layout>
