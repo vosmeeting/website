4520893a71861015a3582459b74037eb38694e18
Phil Armour <phil.armour@gmail.com>
2022-03-10 08:26:00 -0500

    suggested text edits

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,23 +26,28 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="grid min-h-screen grid-cols-1 gap-y-12 rounded-lg bg-white p-4 sm:p-16 shadow-sm">
-        <section className="grid grid-cols-1 sm:grid-cols-2 gap-12">
+      <div className="grid min-h-screen grid-cols-1 gap-y-12 rounded-lg bg-white p-4 shadow-sm sm:p-16">
+        <section className="grid grid-cols-1 gap-12 sm:grid-cols-2">
           <div className="sm:text-justify">
-            <Title className="leading-normal">The conference</Title>
+            <Title className="leading-normal">The Conference</Title>
             <P>
-              This long awaited event will be an innovative meeting, with a
-              dynamic format developed to encourage a candid debate among
-              Veterinary Ophthalmologists with interests in Surgery.
+              We are excited to announce the <b>4th</b> Veterinary Ophthalmic
+              Surgery Meeting. This long awaited event will be an innovative
+              meeting, with a dynamic format developed to encourage a candid
+              debate among Veterinary Ophthalmologists with interests in
+              Surgery.
             </P>
             <br />
             <P>
               This is a limited-space event that will take place on July 22-24
-              <sup>th</sup>, 2022 in Chicago, IL. Come be part of an interactive
-              gathering that will challenge and inspire young professionals and
-              engage the most renowned clinicians with compelling case
-              discussions, pertinent peals, updates from human meetings, and
-              also a Keynote Lecture!
+              <sup>th</sup>, 2022 in Chicago, IL.
+            </P>
+            <br />
+            <P>
+              Come be part of an interactive gathering that will challenge and
+              inspire young professionals and engage the most renowned
+              clinicians with compelling case discussions, pertinent peals,
+              updates from human meetings, and also a Keynote Lecture!
             </P>
           </div>
 
@@ -73,26 +78,35 @@ const Home: NextPage = () => {
           </div>
         </section>
 
-        <section className="grid grid-cols-1 sm:grid-cols-2 text-center sm:text-left gap-x-12">
-          <Title className="font-normal col-span-full">Location</Title>
+        <section className="grid grid-cols-1 gap-x-12 text-center sm:grid-cols-2 sm:text-left">
+          <Title className="col-span-full font-normal">Location</Title>
 
           <div className="relative">
             <Image src={hyatt} layout="fill" objectFit="cover" alt="haytt" />
           </div>
           <div className="">
             <div>
-              <h2 className="pb-2 text-xl sm:text-3xl text-vosm-blue underline underline-offset-4">
-                Hyatt Regency O'hare
+              <h2 className="pb-2 text-xl text-vosm-blue underline underline-offset-4 sm:text-3xl">
+                <a
+                  href="https://www.hyatt.com/hotel/illinois/hyatt-regency-ohare/chiro"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Hyatt Regency O'Hare
+                </a>
               </h2>
               <h3 className=" font-sans  text-sm text-slate-600">
                 9300 Bryn Mawr Avenue <br />
                 Rosemont, Illinois 60018
               </h3>
+              (5 minutes from the airport)
             </div>
+            <br />
             <div>
-              <h2 className="pb-2 text-xl sm:text-2xl text-vosm-blue underline underline-offset-4">
-                Alternative nearby hotels
+              <h2 className="pb-2 text-xl text-vosm-blue underline underline-offset-4 sm:text-2xl">
+                Alternative Hotels
               </h2>
+              (literally down the block)
               <div className="flex justify-center sm:block">
                 <List>
                   {['Hilton Rosemont', 'Embassy Suites', 'Double Tree'].map(
@@ -109,12 +123,12 @@ const Home: NextPage = () => {
             </div>
           </div>
         </section>
-        <section className="flex items-center flex-col gap-y-6 sm:gap-y-12">
+        <section className="flex flex-col items-center gap-y-6 sm:gap-y-12">
           <Title className="text-center sm:text-left">Sponsors</Title>
           <ul className="flex flex-wrap justify-center gap-10">
-            {Array.from({ length: 5 }).map((sponsor, i) => (
+            {Array.from({ length: 3 }).map((sponsor, i) => (
               <li
-                className={`flex items-center justify-center bg-vosm-light-blue text-slate-50 rounded-2xl`}
+                className={`flex items-center justify-center rounded-2xl bg-vosm-light-blue text-slate-50`}
                 style={{ width: 200, height: 200 }}
                 key={i}
               >
@@ -122,11 +136,6 @@ const Home: NextPage = () => {
               </li>
             ))}
           </ul>
-          <Link href="/sponsor" passHref>
-            <Button primary size="large">
-              Become a sponsor
-            </Button>
-          </Link>
         </section>
       </div>
     </Page>
