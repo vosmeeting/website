230050a060d61283dc7d32a80d181890d91c752f
Neo <neo@fauzans-MacBook-Pro.local>
2022-03-14 09:52:07 +0700

    registration form

---
+++ components/icons/index.tsx
@@ -0,0 +1,20 @@
+export function CheckIcon({ size = 25 }) {
+  return (
+    <svg
+      width={size}
+      height={(24 / 25) * size}
+      viewBox="0 0 25 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12.5 0C5.876 0 0.5 5.376 0.5 12C0.5 18.624 5.876 24 12.5 24C19.124 24 24.5 18.624 24.5 12C24.5 5.376 19.124 0 12.5 0ZM10.1 18L4.1 12L5.792 10.308L10.1 14.604L19.208 5.496L20.9 7.2L10.1 18Z"
+        fill="#084F86"
+      />
+      <path
+        d="M10.1 18L4.1 12L5.792 10.308L10.1 14.604L19.208 5.496L20.9 7.2L10.1 18Z"
+        fill="white"
+      />
+    </svg>
+  )
+}
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,88 +1,218 @@
-import { Banner, Card, Layout, Page } from '@shopify/polaris'
-import { notEmpty, useField, useForm } from '@shopify/react-form'
-import { useRouter } from 'next/router'
+import {
+  Banner,
+  Button,
+  ButtonGroup,
+  Card,
+  Form,
+  FormLayout,
+  Heading,
+  Layout,
+  Page,
+  Select,
+  TextField,
+} from '@shopify/polaris'
+import {
+  notEmpty,
+  useDynamicList,
+  useField,
+  useForm,
+} from '@shopify/react-form'
+import { useEffect, useMemo, useState } from 'react'
+import axios from 'axios'
 import * as yup from 'yup'
-import createCheckOutSession from '../services/stripe'
-import Error from 'next/error'
-import ComingSoon from '../components/ComingSoon'
+import { RegistrationTypeList } from './../components/RegistrationType'
+import { Country } from './api/get-countries'
+import {
+  defaultRegistrationType,
+  registrationType as registrationTypes,
+} from './constants/registrationType'
+import { Price } from '../utils/const'
 
-export default function Sponsor() {
-  const schema = {
-    fullName: useField({
-      value: '',
-      validates: [notEmpty('company name is required')],
-    }),
-    email: useField({
-      value: '',
-      validates: [
-        (input) => {
-          try {
-            yup
-              .string()
-              .required('Company/Contact email is required')
-              .email('please provide a valid email')
-              .validateSync(input)
-          } catch (e) {
-            return e.message
-          }
-        },
-      ],
-    }),
+interface PersonalInformation {
+  fullName: string
+  organization: string
+  country: string
+  email: string
+  registrationType: string
+}
+
+function personalInformationFactory(
+  props: Partial<PersonalInformation>
+): PersonalInformation {
+  return {
+    country: props.country || '',
+    email: props.email || '',
+    fullName: props.fullName || '',
+    organization: props.organization || '',
+    registrationType: props.registrationType || '',
   }
+}
 
-  const { fields, submit, submitting, submitErrors } = useForm({
-    fields: schema,
+const ErrorBanner = ({ errors }) => {
+  return errors.length > 0 ? (
+    <Layout.Section>
+      <Banner status="critical">
+        <p>There were some issues with your form submission:</p>
+        <ul>
+          {errors.map(({ message }, index) => {
+            return <li key={`${message}${index}`}>{message}</li>
+          })}
+        </ul>
+      </Banner>
+    </Layout.Section>
+  ) : null
+}
+
+export default function Register() {
+  const [remoteErrors, setRemoteErrors] = useState(null)
+  const [countries, setCountries] = useState<Country[]>([
+    { country: 'United States', abbreviation: 'US' },
+  ])
+  useEffect(() => {
+    axios.get<Country[]>('/api/get-countries').then((res) => {
+      setCountries(res.data)
+    })
+  }, [])
+
+  const personalInformations = useDynamicList(
+    {
+      list: [
+        personalInformationFactory({
+          email: '',
+          country: 'US',
+          registrationType: defaultRegistrationType.value,
+        }),
+      ],
+      validates: {
+        email: [
+          (input) => {
+            try {
+              yup
+                .string()
+                .required('Company/Contact email is required')
+                .email('please provide a valid email')
+                .validateSync(input)
+            } catch (e) {
+              return e.message
+            }
+          },
+        ],
+        organization: [notEmpty('clinic/school/company name is required')],
+        country: [notEmpty('country is required')],
+        fullName: [notEmpty('your name is required')],
+        registrationType: [notEmpty('Registration type is required')],
+      },
+    },
+    personalInformationFactory
+  )
+  const form = useForm({
+    fields: {
+      persons: personalInformations.fields,
+    },
     async onSubmit(form) {
       let remoteErrors = []
-
       try {
-        await createCheckOutSession({
-          ...form,
-          images: [`${process.env.NEXT_PUBLIC_HOST}/vosm_logo.png`],
-        })
+        // call submit service
+        await new Promise((resolve, reject) =>
+          setTimeout(() => reject(new Error('server error')), 3000)
+        )
       } catch (e) {
-        remoteErrors.push(e) // your API call goes here
+        remoteErrors.push(e)
       }
-
-      if (remoteErrors.length > 0) {
+      if (remoteErrors.length) {
+        setRemoteErrors([remoteErrors])
         return { status: 'fail', errors: remoteErrors }
       }
 
       return { status: 'success' }
     },
   })
+  const totalPrice = useMemo(() => {
+    return form.fields.persons.reduce((totalPrice, person) => {
+      const price = registrationTypes.find(
+        (r) => r.value === person.registrationType.value
+      ).price
 
-  const router = useRouter()
+      return totalPrice + price
+    }, 0)
+  }, [form.fields.persons])
 
-  const errorBanner =
-    submitErrors.length > 0 ? (
-      <Layout.Section>
-        <Banner status="critical">
-          <p>There were some issues with your form submission:</p>
-          <ul>
-            {submitErrors.map(({ message }, index) => {
-              return <li key={`${message}${index}`}>{message}</li>
-            })}
-          </ul>
-        </Banner>
-      </Layout.Section>
-    ) : null
-
-  return <ComingSoon />
   return (
     <Page
+      title="Participant Registration"
       subtitle="4th Veterinary	Ophthalmic	Surgery	Meeting	&bull; Jul	22-24, 2022"
       additionalMetadata="Hyatt	Regency	O’Hare,	Rosemont,	IL"
-      narrowWidth
     >
       <Layout>
+        {remoteErrors && <ErrorBanner errors={remoteErrors} />}
         <Layout.Section>
-          <Card title="Participant information" sectioned>
-            <Banner
-              status="warning"
-              title="This page is work in progress"
-              action={{ content: 'Go Home', onAction: () => router.push('/') }}
-            ></Banner>
+          <Card
+            sectioned
+            title="Personal Information"
+            primaryFooterAction={{
+              content: 'register ' + new Price(totalPrice).toDollar(),
+              onAction: form.submit,
+              loading: form.submitting,
+              disabled: !form.dirty,
+            }}
+            secondaryFooterActions={[
+              {
+                content: 'add more participant',
+                onAction: () =>
+                  personalInformations.addItem({
+                    email: 'person@gmail.com',
+                    registrationType: defaultRegistrationType.value,
+                  }),
+              },
+            ]}
+          >
+            <Form onSubmit={form.submit}>
+              {personalInformations.fields.map((field, i) => {
+                return (
+                  <Card sectioned key={i} footerActionAlignment="left">
+                    <FormLayout>
+                      <Heading>Person {i + 1}</Heading>
+                      <RegistrationTypeList
+                        choiceList={registrationTypes}
+                        selected={field.registrationType.value}
+                        onChange={field.registrationType.onChange}
+                        errorMessage={field.registrationType.error}
+                      />
+                      <FormLayout.Group>
+                        <TextField
+                          autoComplete="fullname"
+                          label="Your name"
+                          {...field.fullName}
+                        />
+                        <TextField
+                          autoComplete="email"
+                          label="Email address"
+                          {...field.email}
+                        />
+                      </FormLayout.Group>
+                      <FormLayout.Group>
+                        <TextField
+                          autoComplete="company"
+                          label="Clinic/School/Company"
+                          {...field.organization}
+                        />
+
+                        <Select
+                          label="Country"
+                          options={countries.map(
+                            ({ country, abbreviation }) => ({
+                              label: country,
+                              value: abbreviation,
+                            })
+                          )}
+                          {...field.country}
+                        />
+                      </FormLayout.Group>
+                    </FormLayout>
+                  </Card>
+                )
+              })}
+            </Form>
           </Card>
         </Layout.Section>
       </Layout>
