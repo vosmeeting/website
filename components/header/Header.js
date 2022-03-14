f9587f6c54e6d8b1ee1294f87811a0023f33d80d
Phil Armour <philarmour@users.noreply.github.com>
2022-03-14 00:19:01 -0400

    Home Page Edits - Internal Feedback (#5)
    * suggested text edits
    
    * added hotel detail about special rates

--- components/header/Header.js
+++ components/header/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
   return (
     <div className={styles.main}>
       <Image
-        className="mix-blend-color-burn z-0"
+        className="z-0 mix-blend-color-burn"
         alt="building"
         src={bg}
         layout="fill"
@@ -25,21 +25,17 @@ const Header = () => {
           <Image layout="responsive" alt="logo" src={logo} />
         </div>
 
-        <div className="text-slate-50 font-normal text-center sm:text-left">
+        <div className="text-center font-normal text-slate-50 sm:text-left">
           <p className="text-xl">We are excited to announce</p>
-          <h1 className=" text-xl sm:text-4xl font-semibold">
+          <h1 className=" text-xl font-semibold sm:text-4xl">
             4th Veterinary Ophthalmic Surgery Meeting
           </h1>
-          <p className="text-xl font-sans font-normal">
+          <p className="font-sans text-xl font-normal">
             July 22-24<sup>th</sup> 2022, Chicago, IL
           </p>
         </div>
       </div>
-      <div className={'button-group'}>
-        <Button monochrome>
-          <Link href={'/register'}>Register Now</Link>
-        </Button>
-      </div>
+      <div className={'button-group'}>Registration Opens Soon</div>
     </div>
   )
 }
