a45e6f8069b261b4956cd338d48d58830f313d4f
Neo <neo@fauzans-MacBook-Pro.local>
2022-03-16 07:21:58 +0700

    internal feedbacks

--- components/RegistrationType.tsx
+++ components/RegistrationType.tsx
@@ -5,6 +5,7 @@ import { CheckIcon } from './icons'
 export function RegistrationType({
   priceInDollar = 0,
   label,
+  optionalComment,
   checked,
   onChange,
   value,
@@ -12,42 +13,45 @@ export function RegistrationType({
 }) {
   const iconSize = 24
   return (
-    <label
-      className={classNames(
-        'shadow-sm p-2 flex justify-center gap-1 flex-col text-center rounded-lg relative',
-        {
-          'border-2 shadow-xl border-vosm-blue': checked,
-          border: !checked,
-        }
+    <div className="mb-8">
+      <label
+        className={classNames(
+          'shadow-sm p-2 flex justify-center gap-1 flex-col text-center rounded-lg relative h-full',
+          {
+            'border-2 shadow-xl border-vosm-blue': checked,
+            border: !checked,
+          }
+        )}
+      >
+        {checked && (
+          <div className={`absolute -top-3 -right-3`}>
+            <CheckIcon size={iconSize} />
+          </div>
+        )}
+        {label && <p className="font-normal text-slate-600">{label}</p>}
+        <p className={classNames('font-bold text-vosm-blue')}>
+          {new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+          }).format(priceInDollar)}
+        </p>
+        <input
+          type="radio"
+          name="participant"
+          className="hidden"
+          checked={checked}
+          onChange={(e) => {
+            e.preventDefault()
+            onChange(e.target.value)
+          }}
+          value={value}
+          {...props}
+        />
+      </label>
+      {optionalComment && (
+        <p className="text-slate-500 text-xs italic mt-2">{optionalComment}</p>
       )}
-    >
-      {checked && (
-        <div
-          className={`absolute -top-${iconSize / 8} -right-${iconSize / 8} `}
-        >
-          <CheckIcon size={iconSize} />
-        </div>
-      )}
-      {label && <p className="font-normal text-slate-600">{label}</p>}
-      <p className={classNames('font-bold text-vosm-blue')}>
-        {new Intl.NumberFormat('en-US', {
-          style: 'currency',
-          currency: 'USD',
-        }).format(priceInDollar)}
-      </p>
-      <input
-        type="radio"
-        name="participant"
-        className="hidden"
-        checked={checked}
-        onChange={(e) => {
-          e.preventDefault()
-          onChange(e.target.value)
-        }}
-        value={value}
-        {...props}
-      />
-    </label>
+    </div>
   )
 }
 
@@ -58,12 +62,13 @@ export function RegistrationTypeList({
   errorMessage,
 }) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-4 gap-4">
-      {choiceList.map(({ label, value, price }) => {
+    <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-4">
+      {choiceList.map(({ label, value, price, optionalComment }) => {
         return (
           <RegistrationType
             key={value}
             value={value}
+            optionalComment={optionalComment}
             label={label}
             priceInDollar={price}
             checked={value === selected}
