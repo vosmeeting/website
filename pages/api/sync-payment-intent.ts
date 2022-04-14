c9fec61e45112bc4296da5166653213f036f6207
Neo <neo@fauzans-MacBook-Pro.local>
2022-04-14 23:58:33 +0700

    restrict to POST method

--- pages/api/sync-payment-intent.ts
+++ pages/api/sync-payment-intent.ts
@@ -4,43 +4,45 @@ import { stripe } from './constants/stripe.instance'
 import { updatePaymentIntent } from './webhook'
 
 export default async function handler(req, res) {
-  try {
-    const sessions: {
-      data: { session: { id: string } }[]
-    } = await serverClient.query(
-      q.Map(
-        q.Paginate(q.Documents(q.Collection('checkout_sessions')), {
-          size: 99999,
-        }),
-        q.Lambda(
-          'cs',
-          q.Let(
-            {
-              session: q.Select(['data'], q.Get(q.Var('cs'))),
-            },
-            {
-              session: q.Var('session'),
-            }
+  if (req.method === 'POST') {
+    try {
+      const sessions: {
+        data: { session: { id: string } }[]
+      } = await serverClient.query(
+        q.Map(
+          q.Paginate(q.Documents(q.Collection('checkout_sessions')), {
+            size: 99999,
+          }),
+          q.Lambda(
+            'cs',
+            q.Let(
+              {
+                session: q.Select(['data'], q.Get(q.Var('cs'))),
+              },
+              {
+                session: q.Var('session'),
+              }
+            )
           )
         )
       )
-    )
 
-    const paymentIntentsResult = await Promise.allSettled(
-      sessions.data.map(({ session }) =>
-        stripe.paymentIntents.retrieve(session.paymentIntent)
+      const paymentIntentsResult = await Promise.allSettled(
+        sessions.data.map(({ session }) =>
+          stripe.paymentIntents.retrieve(session.paymentIntent)
+        )
       )
-    )
 
-    const updateResults = await Promise.all(
-      paymentIntentsResult
-        .filter((res) => res.status === 'fulfilled')
-        .map((res) => res.value)
-        .map((pi) => updatePaymentIntent(pi))
-    )
+      const updateResults = await Promise.all(
+        paymentIntentsResult
+          .filter((res) => res.status === 'fulfilled')
+          .map((res) => res.value)
+          .map((pi) => updatePaymentIntent(pi))
+      )
 
-    res.send(updateResults)
-  } catch (e) {
-    res.status(500).send(e)
+      res.send(updateResults)
+    } catch (e) {
+      res.status(500).send(e)
+    }
   }
 }
