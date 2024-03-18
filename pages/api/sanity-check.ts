e1b2b6e3451e7fd977086d29269a4f004ea30053
Neo <neo@fauzans-MacBook-Pro.local>
2024-03-18 10:50:37 +0700

    add err logging

--- pages/api/sanity-check.ts
+++ pages/api/sanity-check.ts
@@ -1,10 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { mongoDatabaseService } from '../../infra/database/mongo-database-service/MongoDatabaseService';
 const sanityCheck = async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log('sanity checking by getting meeting by title');
-  const result = await mongoDatabaseService.getLatestMeeting();
-  console.log('result', result);
+  try {
+    console.log('sanity checking by getting meeting by title');
+    const result = await mongoDatabaseService.getLatestMeeting();
+    console.log('result', result);
 
-  res.status(200).json({ message: 'Sanity check passed', latestMeeting: result });
+    res.status(200).json({ message: 'Sanity check passed', latestMeeting: result });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json(e);
+  }
 };
 export default sanityCheck;
